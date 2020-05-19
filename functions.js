c5ccef8e0f93394a8a4e9c7a6c8a8df9d5d5592a
kalcalen <kalcalen@gmail.com>
2020-05-18 22:59:23 -0700

    Created basic functions

--- functions.js
+++ functions.js
@@ -4,3 +4,43 @@ function add(input1, input2) {
    // R: a single JavaScript value
    return input1 + input2;
 }
+
+function subtract(firstNumber, secondNumber) {
+   // A1: a number
+   // A2: a number
+   // R: a number
+   return secondNumber - firstNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+   // A1: a Number
+   // A2: a Number
+   // R: a Number
+   return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+   // A1: a Number
+   // A2: a Number
+   // R: a Number
+   return firstNumber / secondNumber;
+}
+
+function Increment(firstNumber) {
+   // A1: a Number
+   // R: a Number added buy 1 using +
+   return ++firstNumber;
+}
+
+function Decrement(firstNumber) {
+   // A1: a Number
+   // R: a Number subtracted by 1 using -
+   return --firstNumber;
+}
+
+function decimal(firstNumber, secondNumber) {
+   // A1: a Number
+   // A2: a Number
+   // R: a Decimal number
+   return firstNumber * secondNumber;
+}
